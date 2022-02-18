128ea65be739171649d775c66a015c4152f4c44d
Edie Shana <eshana@student.21-school.ru>
2022-02-18 14:08:21 +0300

    cd: added PWD & OLDPWD rewriting

--- src/builtins/cd.c
+++ src/builtins/cd.c
@@ -6,21 +6,68 @@
 /*   By: eshana <eshana@student.21-school.ru>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/04 09:25:47 by eshana            #+#    #+#             */
-/*   Updated: 2022/02/09 01:06:01 by eshana           ###   ########.fr       */
+/*   Updated: 2022/02/18 13:58:46 by eshana           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+char	*ft_get_env_addr(const char *key)
+{
+	int		i;
+	int		res;
+
+	i = 0;
+	while (g_env[i])
+	{
+		res = ft_strncmp(g_env[i], key, ft_strlen(key));
+		if (!res)
+			return (g_env[i]);
+		i++;
+	}
+	return (NULL);
+}
+
+void	ft_rewrite_pwd(char *pwd, char *oldpwd)
+{
+	char	*tmp;
+	int		i;
+
+	i = 0;
+	while (g_env[i])
+	{
+		if (!ft_strncmp(g_env[i], "PWD=", ft_strlen("PWD=")))
+		{
+			free(g_env[i]);
+			g_env[i] = pwd;
+		}
+		if (!ft_strncmp(g_env[i], "OLDPWD=", ft_strlen("OLDPWD=")))
+		{
+			free(g_env[i]);
+			g_env[i] = oldpwd;
+		}
+		i++;
+	}
+}
+
 int	ft_cd(char **argv)
 {
-	int	ret;
+	int		ret;
+	char	*pwd;
+	char 	*oldpwd;
+	char 	*buf;
 
 	if (argv[1])
 		ret = chdir(argv[1]);
 	if (!ret)
 	{
 		//rewrite PWD & OLDPWD
+		pwd = ft_get_env_addr("PWD=");
+		oldpwd = ft_strjoin("OLD", pwd);
+		buf = (char *)malloc(sizeof(char) * (PATH_MAX + 1));
+		pwd = ft_strjoin("PWD=", getcwd(buf, PATH_MAX));
+		free(buf);
+		ft_rewrite_pwd(pwd, oldpwd);
 		return (0);
 	}
 	ft_put_err("minishell: cd: ");
